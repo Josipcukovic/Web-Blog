5823e328e2928b92b7382521c5660516c1bcbb3f
Josipcukovic <cukone1@gmail.com>
2021-08-11 21:13:59 +0200

    refactoring

--- myProfile.js
+++ myProfile.js
@@ -6,26 +6,7 @@ const profilePhoto = document.querySelector("#profilePhoto");
 const myPhoto = document.querySelector(".mojaSlika");
 const main = document.querySelector("main");
 
-
-
-// blogRef.orderBy("created_at", "desc").limit(20).onSnapshot(snapshot => {
-//     let changes = snapshot.docChanges().reverse();
-//     changes.forEach(doc => {
-
-//         if (doc.type == "added") {
-//             renderBlogData(doc.doc);
-//         } else if (doc.type == "removed") {
-//             const target = document.getElementById(doc.doc.id);
-//             target.nextElementSibling.remove()
-//             target.remove();
-//         }
-//     })
-// })
-
-
-
-
-blogRef.where("created_by_id", "==", id).get().then(blogs => {
+blogRef.where("created_by_id", "==", id).orderBy("created_at", "asc").get().then(blogs => {
     // console.log(auth.currentUser.displayName);
     // const data = document.data();
     blogs.docs.forEach(blog => {
@@ -57,26 +38,6 @@ blogRef.where("created_by_id", "==", id).get().then(blogs => {
         dohvatiKomentare(blog.id);
     })
 
-    // const blogTemplate = ` <li class="blog-list-element" id=${document.id}> 
-    //   <p class ="author">Written by: ${data.created_by}</p>
-    //   <span>${data.title}</span>
-    //   <img src="${data.picture != null ? data.picture : cat}" alt="#">
-    //     <span>${data.body}</span> 
-    //     <div class = "tooltip"> ${created_at.toLocaleDateString()} at ${created_at.toLocaleTimeString()} </div>
-    //     <p class="createdAt" onmouseover="toggleTimeCreated()" onmouseleave="toggleTimeCreated()"> ${timeAgo} </p> 
-    //     <p class="commentPost">Comment this post</p>
-    //     <div class='delete' >X</div>
-
-    //     </li> 
-
-    //     <div class ="commentSection" >
-    //     <ul class="commentsDisplay"> </ul>
-    //     <form class="commentForm">
-    //       <input type="text" name="comment" placeholder="Your comment..." class="comment">
-    //     </form>
-    //     </div> `
-    // blogList.insertAdjacentHTML('afterbegin', blogTemplate);
-    // dohvatiKomentare(document.id);
 
 });
 
@@ -84,13 +45,6 @@ userRef.doc(id).get().then(doc => {
     console.log(doc.data());
     const data = doc.data();
     const link = "cat.jpg"
-    // const template = `
-    // <p id="name">Your name: ${data.ime} ${data.prezime}</p>
-    // <img src="${user.photoURL != null ? user.photoURL : link}" alt="#" width="250" height="250" class="mojaSlika">
-    // <input type="file" value="upload" id="profilePhoto" />
-    // <p id="email">Your email: ${data.email}</p>
-    // <ul class="userPosts"></ul>`
-
     const name = document.querySelector("#name");
     const email = document.querySelector("#email");
 
@@ -127,26 +81,3 @@ profilePhoto.addEventListener("change", e => {
 
 
 });
-
-
-
-// photo.addEventListener("change", (e) => {
-//     var currentUser = firebase.auth().currentUser;
-//     const file = e.target.files[0];
-//     const name = file.name;
-//     const metadata = {
-//         contentType: file.type,
-//     };
-//     const Storageref = storageDb.ref("Profile pictures/" + currentUser.uid);
-//     alert("Pričekajte nekoliko trenutaka učitavanje slike profila.");
-
-//     Storageref.child(name).put(file, metadata).then((snapshot) => snapshot.ref.getDownloadURL()).then((link) => {
-//         currentUser.updateProfile({ photoURL: link }).then(function () {
-//             db.ref("/users/" + currentUser.uid).update({
-//                 slika: link,
-//             });
-//             location.reload();
-//         }).catch(function (error) { window.alert(error.message) });
-
-//     });
-// });
